841495d3fa9988702698f58cb39a2b0168244856
Subin-Panta <subinpanta9@gmail.com>
2020-01-23 16:49:05 +0545

    added pages for routing and css data

---
+++ src/pages/SingleRoom.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+class SingleRoom extends React.Component{
+    render(){
+        return(
+        <div>
+            Single rooms         
+        </div>
+    )   
+    }
+}
+export default SingleRoom
